b1ab5c879e4953d87a3c0b116dc36e07e5bba0d4
Thanvi-kasi <kasithanvi01@gmail.com>
2026-05-12 12:44:54 +0530

    Create Matrix Diagonal Sum.py

---
+++ Matrix Diagonal Sum.py
@@ -0,0 +1,12 @@
+class Solution:
+    def diagonalSum(self, mat):
+        n = len(mat)
+        total = 0
+
+        for i in range(n):
+            total += mat[i][i]  
+
+            if i != n - 1 - i:
+                total += mat[i][n - 1 - i]  
+
+        return total
