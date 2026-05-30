7716fd83182e5802f2e451b7e3f1997124fb751b
Thanvi-kasi <kasithanvi01@gmail.com>
2026-05-30 22:11:39 +0530

    Create Sort an Array.py

---
+++ Sort an Array.py
@@ -0,0 +1,29 @@
+from typing import List
+
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+
+        def heapify(i: int, heap_size: int) -> None:
+            largest = i
+            left = 2 * i + 1
+            right = 2 * i + 2
+
+            if left < heap_size and nums[left] > nums[largest]:
+                largest = left
+
+            if right < heap_size and nums[right] > nums[largest]:
+                largest = right
+
+            if largest != i:
+                nums[i], nums[largest] = nums[largest], nums[i]
+                heapify(largest, heap_size)
+
+        for i in range(n // 2 - 1, -1, -1):
+            heapify(i, n)
+
+        for i in range(n - 1, 0, -1):
+            nums[0], nums[i] = nums[i], nums[0]
+            heapify(0, i)
+
+        return nums
